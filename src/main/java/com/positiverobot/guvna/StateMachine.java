70e475cb72c9bb2a5695af436155577212314c99
byeo <oli.github@mail-filter.com>
2015-02-14 17:05:09 +0000

    Support ways of copmuting the next state e.g. a stack

--- src/main/java/com/positiverobot/guvna/StateMachine.java
+++ src/main/java/com/positiverobot/guvna/StateMachine.java
@@ -11,102 +11,107 @@ import org.slf4j.LoggerFactory;
  * Not threadsafe.
  */
 public class StateMachine {
-	private final Logger LOG = LoggerFactory.getLogger(StateMachine.class);
+    private final Logger LOG = LoggerFactory.getLogger(StateMachine.class);
 
-	private Object _currentState;
-	private StateMachinePlan _plan;
-	private Queue<Object> _eventQueue = new LinkedList<Object>();
+    private Object _currentState;
+    private StateMachinePlan _plan;
+    private Queue<Object> _eventQueue = new LinkedList<Object>();
 
-	public StateMachine(StateMachinePlan plan, Object aStartState) {
-		_plan = plan;
-		_currentState = aStartState;
-	}
+    public StateMachine(StateMachinePlan plan, Object aStartState) {
+        _plan = plan;
+        _currentState = aStartState;
+    }
 
-	public Object getState() {
-		return _currentState;
-	}
+    public Object getState() {
+        return _currentState;
+    }
 
-	/**
-	 * @param event will cause a runtime exception
-	 */
-	public void queue(Object event) {
-		if (event == null) {
-			throw new IllegalArgumentException("Null events are invalid");
-		}
-		_eventQueue.add(event);
-	}
+    /**
+     * @param event
+     *            will cause a runtime exception
+     */
+    public void queue(Object event) {
+        if (event == null) {
+            throw new IllegalArgumentException("Null events are invalid");
+        }
+        _eventQueue.add(event);
+    }
 
-	private void performTransition(Object nextState, Object event) {
-		Action leaveAction = _plan._leaveActions.get(_currentState);
-		if (leaveAction != null) {
-			leaveAction.apply(this, event, nextState);
-		}
+    private void performTransition(Object nextState, Object event) {
+        Action leaveAction = _plan._leaveActions.get(_currentState);
+        if (leaveAction != null) {
+            leaveAction.apply(this, event, nextState);
+        }
 
-		Action entryAction = _plan._entryActions.get(nextState);
-		if (entryAction != null) {
-			entryAction.apply(this, event, nextState);
-		}
-		// entry actions performed before state change
-		_currentState = nextState;
-	}
+        Action entryAction = _plan._entryActions.get(nextState);
+        if (entryAction != null) {
+            entryAction.apply(this, event, nextState);
+        }
+        // entry actions performed before state change
+        _currentState = nextState;
+    }
 
-	/**
-	 * 
-	 * @return true if more exist
-	 */
-	public boolean processNextEvent() {
-		Object event = _eventQueue.poll();
+    /**
+     * 
+     * @return true if more exist
+     */
+    public boolean processNextEvent() {
+        Object event = _eventQueue.poll();
 
-		// Ignore null events as we can't tell if they're real
-		// or indicate the queue is empty
-		if(event == null) {
-			return hasMoreEvents();
-		}
-		
-		int indexOfEvent = _plan._inputs.indexOf(event);
+        // Ignore null events as we can't tell if they're real
+        // or indicate the queue is empty
+        if (event == null) {
+            return hasMoreEvents();
+        }
 
-		if (indexOfEvent >= 0) {
-			// don't ignore loopback event in column zero
+        int indexOfEvent = _plan._inputs.indexOf(event);
 
-			LOG.debug("{} received event type:[{}]", this, event);
+        if (indexOfEvent >= 0) {
+            // don't ignore loopback event in column zero
 
-			List<?> list = _plan._transitions.get(_currentState);
-			if (list != null) {
-				if (list.size() > indexOfEvent) {
-					Object nextState = list.get(indexOfEvent);
-					if (nextState != null) {
-						performTransition(nextState, event);
-						LOG.debug("{} processed event type:[{}]", this, event);
-					} else {
-						LOG.debug(
-								"{} has no transition for registered event:[{}]",
-								this, event);
-					}
-				} else {
-					LOG.debug("{} has no transition for registered event:[{}]",
-							this, event);
-				}
-			} else {
-				LOG.error("Unknown state:[{}]", _currentState);
-			}
-		} else {
-			LOG.error("{} received Unknown event type:[{}]", this, event);
-		}
-		return hasMoreEvents();
-	}
+            LOG.debug("{} received event type:[{}]", this, event);
 
-	public boolean hasMoreEvents() {
-		return _eventQueue.peek() != null;
-	}
+            List<?> list = _plan._transitions.get(_currentState);
+            if (list != null) {
+                if (list.size() > indexOfEvent) {
+                    Object nextState = computeNextState(list.get(indexOfEvent));
+                    if (nextState != null) {
+                        performTransition(nextState, event);
+                        LOG.debug("{} processed event type:[{}]", this, event);
+                    } else {
+                        LOG.debug(
+                                "{} has no transition for registered event:[{}]",
+                                this, event);
+                    }
+                } else {
+                    LOG.debug("{} has no transition for registered event:[{}]",
+                            this, event);
+                }
+            } else {
+                LOG.error("Unknown state:[{}]", _currentState);
+            }
+        } else {
+            LOG.error("{} received Unknown event type:[{}]", this, event);
+        }
+        return hasMoreEvents();
+    }
 
-	public void processOutstandingEvents() {
-		while (processNextEvent()) {
-		}
-	}
+    protected Object computeNextState(Object object) {
+        return object;
+    }
 
-	@Override
-	public String toString() {
-		return "StateMachine [_currentState=" + _currentState
-				+ ", _eventQueue=" + _eventQueue + "]";
-	}
+    public boolean hasMoreEvents() {
+        return _eventQueue.peek() != null;
+    }
+
+    public void processOutstandingEvents() {
+        while (processNextEvent()) {
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "StateMachine [_currentState=" + _currentState
+                + ", _eventQueue=" + _eventQueue + "]";
+    }
 }
